d5e7423545c7a164b11d52fde5bcfccb2fb386a7
Valentin Chaboche <valentin.chaboche@nomadic-labs.com>
2026-01-19 15:39:37 +0100

    refactor(installer/snapshot): use 'open' instead of 'include' for Helpers
    Addresses review feedback from @vch9

--- src/installer/snapshot.ml
+++ src/installer/snapshot.ml
@@ -1,13 +1,13 @@
 (******************************************************************************)
 (*                                                                            *)
 (* SPDX-License-Identifier: MIT                                               *)
-(* Copyright (c) 2025 Nomadic Labs <contact@nomadic-labs.com>                 *)
+(* Copyright (c) 2026 Nomadic Labs <contact@nomadic-labs.com>                 *)
 (*                                                                            *)
 (******************************************************************************)
 
 open Rresult
 open Installer_types
-include Helpers
+open Helpers
 
 let history_mode_matches ~requested ~snapshot_mode =
   let requested_str = History_mode.to_string requested in
